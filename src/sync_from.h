c80918b02fe259e57afe34c741e647a26ce110aa
Will Bryant <will.bryant@gmail.com>
2013-04-02 00:58:03 +1300

    send the protobuf Database over the wire

--- src/sync_from.h
+++ src/sync_from.h
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <stdexcept>
 #include <unistd.h>
 
 #include "kitchen_sync.pb.h"
@@ -7,15 +8,12 @@ using namespace std;
 
 template<class T>
 void sync_from(T &client) {
-	kitchen_sync::Database database(client.database_schema());
-	for (int t = 0; t < database.table_size(); t++) {
-		const kitchen_sync::Table &table(database.table(t));
-		cout << table.name() << endl;
-		for (int c = 0; c < table.column_size(); c++) {
-			const kitchen_sync::Column &column(table.column(c));
-			cout << "\t" << column.name() << endl;
-		}
+	kitchen_sync::Database from_database(client.database_schema());
+
+	if (!from_database.SerializeToOstream(&cout)) {
+		throw runtime_error("Couldn't serialize database");
 	}
+	cout.flush();
 
 	close(STDOUT_FILENO);
 }
--- src/sync_to.h
+++ src/sync_to.h
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <stdexcept>
 #include <unistd.h>
 
 #include "kitchen_sync.pb.h"
@@ -7,6 +8,24 @@ using namespace std;
 
 template<class T>
 void sync_to(T &client) {
+	kitchen_sync::Database to_database(client.database_schema());
+	kitchen_sync::Database from_database;
+
+	if (!from_database.ParseFromFileDescriptor(STDIN_FILENO)) {
+		throw runtime_error("Couldn't read database");
+	}
+
+	for (int t = 0; t < from_database.table_size(); t++) {
+		const kitchen_sync::Table &table(from_database.table(t));
+		cerr << table.name() << endl;
+		for (int c = 0; c < table.column_size(); c++) {
+			const kitchen_sync::Column &column(table.column(c));
+			cerr << "\t" << column.name() << endl;
+		}
+	}
+
+	cerr << test.name() << endl;
+
 	close(STDIN_FILENO);
 	close(STDOUT_FILENO);
 }
