e5f4689f3d15a6d184f0388a0e435067e045a37f
Will Bryant <will.bryant@gmail.com>
2013-12-29 12:54:02 +1300

    tolerate receiving a quit message on error cases; will be needed when concurrency goes in, and harmless before then too

--- test/schema_to_test.rb
+++ test/schema_to_test.rb
@@ -41,6 +41,7 @@ class SchemaToTest < KitchenSync::EndpointTestCase
     create_footbl
 
     expects(:schema).with().returns([{"tables" => []}])
+    stubs(:quit)
     expect_stderr("Extra table footbl") do
       receive_commands
     end
@@ -50,6 +51,7 @@ class SchemaToTest < KitchenSync::EndpointTestCase
     clear_schema
 
     expects(:schema).with().returns([{"tables" => [footbl_def]}])
+    stubs(:quit)
     expect_stderr("Missing table footbl") do
       receive_commands
     end
@@ -61,6 +63,7 @@ class SchemaToTest < KitchenSync::EndpointTestCase
     create_secondtbl
 
     expects(:schema).with().returns([{"tables" => [footbl_def, middletbl_def, secondtbl_def]}])
+    stubs(:quit)
     expect_stderr("Missing table footbl") do
       receive_commands
     end
@@ -72,6 +75,7 @@ class SchemaToTest < KitchenSync::EndpointTestCase
     create_secondtbl
 
     expects(:schema).with().returns([{"tables" => [footbl_def, middletbl_def, secondtbl_def]}])
+    stubs(:quit)
     expect_stderr("Missing table middletbl") do
       receive_commands
     end
@@ -83,6 +87,7 @@ class SchemaToTest < KitchenSync::EndpointTestCase
     create_middletbl
 
     expects(:schema).with().returns([{"tables" => [footbl_def, middletbl_def, secondtbl_def]}])
+    stubs(:quit)
     expect_stderr("Missing table secondtbl") do
       receive_commands
     end
@@ -95,6 +100,7 @@ class SchemaToTest < KitchenSync::EndpointTestCase
     create_secondtbl
 
     expects(:schema).with().returns([{"tables" => [middletbl_def, secondtbl_def]}])
+    stubs(:quit)
     expect_stderr("Extra table footbl") do
       receive_commands
     end
@@ -107,6 +113,7 @@ class SchemaToTest < KitchenSync::EndpointTestCase
     create_secondtbl
 
     expects(:schema).with().returns([{"tables" => [footbl_def, secondtbl_def]}])
+    stubs(:quit)
     expect_stderr("Extra table middletbl") do
       receive_commands
     end
@@ -119,6 +126,7 @@ class SchemaToTest < KitchenSync::EndpointTestCase
     create_secondtbl
 
     expects(:schema).with().returns([{"tables" => [footbl_def, middletbl_def]}])
+    stubs(:quit)
     expect_stderr("Extra table secondtbl") do
       receive_commands
     end
@@ -132,6 +140,7 @@ class SchemaToTest < KitchenSync::EndpointTestCase
     execute("CREATE UNIQUE INDEX pri2_key ON secondtbl (pri2)") # needed for pg, which drops the primary key when a column is removed; mysql just removes the removed column from the index
 
     expects(:schema).with().returns([{"tables" => [secondtbl_def]}])
+    stubs(:quit)
     expect_stderr("Missing column pri1 on table secondtbl") do
       receive_commands
     end
@@ -143,6 +152,7 @@ class SchemaToTest < KitchenSync::EndpointTestCase
     execute("ALTER TABLE footbl DROP COLUMN another_col")
 
     expects(:schema).with().returns([{"tables" => [footbl_def]}])
+    stubs(:quit)
     expect_stderr("Missing column another_col on table footbl") do
       receive_commands
     end
@@ -154,6 +164,7 @@ class SchemaToTest < KitchenSync::EndpointTestCase
     execute("ALTER TABLE footbl DROP COLUMN col3")
 
     expects(:schema).with().returns([{"tables" => [footbl_def]}])
+    stubs(:quit)
     expect_stderr("Missing column col3 on table footbl") do
       receive_commands
     end
@@ -165,6 +176,7 @@ class SchemaToTest < KitchenSync::EndpointTestCase
     # postgresql doesn't support BEFORE/AFTER so we do this test by changing the expected schema instead
 
     expects(:schema).with().returns([{"tables" => [footbl_def.merge("columns" => footbl_def["columns"][1..-1])]}])
+    stubs(:quit)
     expect_stderr("Extra column col1 on table footbl") do
       receive_commands
     end
@@ -176,6 +188,7 @@ class SchemaToTest < KitchenSync::EndpointTestCase
     # postgresql doesn't support BEFORE/AFTER so we do this test by changing the expected schema instead
 
     expects(:schema).with().returns([{"tables" => [footbl_def.merge("columns" => footbl_def["columns"][0..0] + footbl_def["columns"][2..-1])]}])
+    stubs(:quit)
     expect_stderr("Extra column another_col on table footbl") do
       receive_commands
     end
@@ -187,6 +200,7 @@ class SchemaToTest < KitchenSync::EndpointTestCase
     # postgresql doesn't support BEFORE/AFTER so we do this test by changing the expected schema instead
 
     expects(:schema).with().returns([{"tables" => [footbl_def.merge("columns" => footbl_def["columns"][0..-2])]}])
+    stubs(:quit)
     expect_stderr("Extra column col3 on table footbl") do
       receive_commands
     end
@@ -198,6 +212,7 @@ class SchemaToTest < KitchenSync::EndpointTestCase
     # postgresql doesn't support BEFORE/AFTER so we do this test by changing the expected schema instead
 
     expects(:schema).with().returns([{"tables" => [footbl_def.merge("columns" => footbl_def["columns"][1..-1] + footbl_def["columns"][0..0])]}])
+    stubs(:quit)
     expect_stderr("Misordered column another_col on table footbl, should have col1 first") do
       receive_commands
     end
@@ -209,6 +224,7 @@ class SchemaToTest < KitchenSync::EndpointTestCase
     create_secondtbl
 
     expects(:schema).with().returns([{"tables" => [secondtbl_def.merge("primary_key_columns" => [0, 1])]}])
+    stubs(:quit)
     expect_stderr("Mismatching primary key (pri2, pri1) on table secondtbl, should have (pri1, pri2)") do
       receive_commands
     end
@@ -219,6 +235,7 @@ class SchemaToTest < KitchenSync::EndpointTestCase
     create_secondtbl
 
     expects(:schema).with().returns([{"tables" => [secondtbl_def.merge("primary_key_columns" => [1, 0, 2])]}])
+    stubs(:quit)
     expect_stderr("Mismatching primary key (pri2, pri1) on table secondtbl, should have (pri2, pri1, sec)") do
       receive_commands
     end
@@ -229,6 +246,7 @@ class SchemaToTest < KitchenSync::EndpointTestCase
     create_secondtbl
 
     expects(:schema).with().returns([{"tables" => [secondtbl_def.merge("primary_key_columns" => [2, 1, 0])]}])
+    stubs(:quit)
     expect_stderr("Mismatching primary key (pri2, pri1) on table secondtbl, should have (sec, pri2, pri1)") do
       receive_commands
     end
