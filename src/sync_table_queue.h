e28a36f5d51bbc5d955a2629a877ada6375f8526
Will Bryant <will.bryant@gmail.com>
2014-01-04 00:35:49 +1300

    rip out the separate input/output sync thread model and the associated shared work queue of hashes from the other end, with sadness.
    that architecture was desirable because it helped get better utilisation of threads locally since we could be hashing rows in a second table while we are waiting for a response to the hash we sent for an earlier table.
    
    however, this necessitated a shared work queue which meant that replacement row data could come back in to any combination of the different workers (albeit only one at a time), and this turns out not to work with innodb's implementation of unique keys, specifically the way that (even in read committed mode) it uses a shared next-key lock to defend against the possibility another thread will try and insert into the same unique key 'hole', and that means we may not be able to replace rows in tables with a unique key if we have already replaced some unrelated rows in the same table in another worker (it will detect a deadlock or simply block until the other worker commits - which in fact it won't do since it is waiting for this worker to finish, meaning the blocked worker eventually times out).
    
    so we have no choice but to go back to the simpler non-pipelined implementation that processes one table at a time (still sharing the queue for the tables not yet being worked on).

--- src/sync_table_queue.h
+++ src/sync_table_queue.h
@@ -1,51 +1,27 @@
 #include <queue>
 #include <boost/thread.hpp>
 
-struct WorkTask {
-	WorkTask(): table(NULL) {}
-	WorkTask(const Table &table): table(&table) {}
-	WorkTask(const Table &table, const ColumnValues &prev_key, const ColumnValues &last_key, const string &hash): table(&table), prev_key(prev_key), last_key(last_key), hash(hash) {}
-
-	const Table *table;
-	ColumnValues prev_key;
-	ColumnValues last_key;
-	string hash;
-
-	bool operator <(const WorkTask &other) const {
-		if (table == other.table) return false;
-		if (!table) return true;
-		return (table->name > other.table->name); // earlier-named tables have higher priority
-	}
-};
-
-struct SyncWorkQueue {
-	SyncWorkQueue(): tables_left(-1), aborted(false) {}
+struct SyncTableQueue {
+	SyncTableQueue(): tables_left(-1), aborted(false) {}
 
 	void enqueue(const Tables &tables) {
 		boost::unique_lock<boost::mutex> lock(queue_mutex);
 		tables_left = tables.size();
 		for (Tables::const_iterator from_table = tables.begin(); from_table != tables.end(); ++from_table) {
-			queue.push(WorkTask(*from_table));
+			queue.push_back(&*from_table);
 		}
 		enqueued.notify_all();
 	}
 
-	void enqueue(const Table &table, const ColumnValues &prev_key, const ColumnValues &last_key, const string &hash) {
-		boost::unique_lock<boost::mutex> lock(queue_mutex);
-		bool was_empty = queue.empty();
-		queue.push(WorkTask(table, prev_key, last_key, hash));
-		if (was_empty) enqueued.notify_one();
-	}
-
-	WorkTask pop() {
-		WorkTask task;
+	const Table* pop() {
+		const Table* table(NULL);
 		boost::unique_lock<boost::mutex> lock(queue_mutex);
 		while (tables_left && queue.empty()) enqueued.wait(lock);
 		if (tables_left) {
-			task = queue.top();
-			queue.pop();
+			table = queue.front();
+			queue.pop_front();
 		}
-		return task;
+		return table;
 	}
 
 	void wait_until_started() {
@@ -71,7 +47,7 @@ struct SyncWorkQueue {
 		return (tables_left == 0);
 	}
 	
-	priority_queue<WorkTask> queue;
+	list<const Table*> queue;
 	boost::mutex queue_mutex;
 	boost::condition_variable enqueued;
 	size_t tables_left;
--- src/sync_to.h
+++ src/sync_to.h
@@ -4,7 +4,7 @@
 #include "schema.h"
 #include "schema_functions.h"
 #include "command.h"
-#include "sync_work_queue.h"
+#include "sync_table_queue.h"
 #include "sync_algorithm.h"
 #include "table_row_applier.h"
 #include "fdstream.h"
@@ -15,192 +15,180 @@ struct sync_error: public runtime_error {
 	sync_error(): runtime_error("Sync error") { }
 };
 
-template <typename DatabaseClient, typename OutputStream>
-struct SyncToOutputWorker {
-	SyncToOutputWorker(
-		SyncWorkQueue &work_queue,
+template <typename DatabaseClient>
+struct SyncToWorker {
+	SyncToWorker(
+		SyncTableQueue &table_queue,
 		const char *database_host, const char *database_port, const char *database_name, const char *database_username, const char *database_password,
-		int write_to_descriptor, bool leader):
-			work_queue(work_queue),
-			client(database_host, database_port, database_name, database_username, database_password, true /* readonly */, false /* no snapshot - don't take gap locks */),
-			output_stream(write_to_descriptor), output(output_stream), leader(leader),
-			worker_thread(boost::ref(*this)) {
-	}
-
-	~SyncToOutputWorker() {
-		worker_thread.join();
-	}
-
-	void operator()() {
-		const int PROTOCOL_VERSION_SUPPORTED = 1;
-
-		try {
-			// tell the other end what version of the protocol we can speak, and have them tell us which version we're able to converse in
-			send_command(output, "protocol", PROTOCOL_VERSION_SUPPORTED);
-
-			// start syncing table data
-			if (leader) {
-				// FUTURE: export the remote snapshot
-
-				// get its schema
-				send_command(output, "schema");
-			} else {
-				// FUTURE: adopt the exported remote snapshot
-			}
-
-			WorkTask task;
-
-			while (true) {
-				// grab the next task off the queue (blocking if it's empty, quitting if there's no more tables to work on)
-				task = work_queue.pop();
-				if (!task.table) break;
-
-				const Table &table(*task.table);
-				ColumnValues matched_up_to_key;
-				size_t rows_to_hash = check_hash_and_choose_next_range(client, table, task.prev_key, task.last_key, task.hash, matched_up_to_key);
-
-				// calculate our hash of the next rows_to_hash rows
-				RowHasherAndLastKey<typename DatabaseClient::RowType> hasher_for_our_rows(table.primary_key_columns);
-				if (rows_to_hash) {
-					client.retrieve_rows(table, matched_up_to_key, rows_to_hash, hasher_for_our_rows);
-				}
-
-				if (hasher_for_our_rows.row_count == 0) {
-					// rows don't match, and there's only one or no rows in that range at our end, so ask the other end to send theirs
-					send_command(output, "rows", table.name, matched_up_to_key, hasher_for_our_rows.last_key /* empty, meaning to the end of the table */);
-
-				} else {
-					// tell the other end to check its hash of the same rows, using key ranges rather than a count to improve the chances of a match.
-					send_command(output, "hash", table.name, matched_up_to_key, hasher_for_our_rows.last_key, hasher_for_our_rows.finish());
-				}
-			}
-		} catch (const exception &e) {
-			cerr << e.what() << endl;
-			work_queue.abort(); // abort all other output threads
-		}
-
-		try {
-			// send a quit so the other end closes its output, needed so our end's input thread terminates; note we do this both for normal completion and also errors
-			send_command(output, "quit");
-		} catch (const exception &e) {
-			if (!work_queue.aborted) cerr << e.what() << endl;
-			work_queue.abort();
-		}
-	}
-
-	SyncWorkQueue &work_queue;
-	DatabaseClient client;
-	OutputStream output_stream;
-	Packer<OutputStream> output;
-	bool leader;
-	boost::thread worker_thread;
-};
-
-template <typename DatabaseClient, typename InputStream>
-struct SyncToInputWorker {
-	SyncToInputWorker(
-		SyncWorkQueue &work_queue,
-		const char *database_host, const char *database_port, const char *database_name, const char *database_username, const char *database_password,
-		int read_from_descriptor, bool leader):
-			work_queue(work_queue),
+		int read_from_descriptor, int write_to_descriptor, bool leader):
+			table_queue(table_queue),
 			client(database_host, database_port, database_name, database_username, database_password, false /* not readonly */, false /* no snapshot - don't take gap locks */),
-			input_stream(read_from_descriptor), input(input_stream), leader(leader),
+			input_stream(read_from_descriptor), input(input_stream), 
+			output_stream(write_to_descriptor), output(output_stream),
+			leader(leader),
+			protocol_version(0),
 			worker_thread(boost::ref(*this)) {
 	}
 
-	~SyncToInputWorker() {
+	~SyncToWorker() {
 		worker_thread.join();
 	}
 
 	void operator()() {
 		try {
-			// read the response to the protocol_version command that the output thread sends when it starts
-			// this is currently unused, but the command's semantics need to be in place for it to be useful in the future...
-			input >> protocol_version;
-
-			if (leader) {
-				// FUTURE: read the remote snapshot
-
-				// read the response to the schema command that the output thread sends when it starts
-				Database from_database;
-				input >> from_database;
-
-				// check they match
-				check_schema_match(from_database, client.database_schema());
-
-				// queue up all the tables, and signal the non-leaders that it's time to start
-				work_queue.enqueue(client.database_schema().tables);
-			} else {
-				work_queue.wait_until_started();
-				// FUTURE: adopt the exported remote snapshot
-			}
+			negotiate_protocol();
+			// FUTURE: export the remote snapshot from the leader to the other workers
+			compare_schema();
+			enqueue_tables();
 
 			client.disable_referential_integrity();
 
 			while (true) {
-				Command command;
+				// grab the next table to work on from the queue (blocking if it's empty)
+				const Table *table = table_queue.pop();
 
-				try {
-					input >> command;
-				} catch (const stream_closed_error &e) {
-					if (!work_queue.check_if_finished_all_tables()) throw;
-					client.enable_referential_integrity();
-					client.commit_transaction();
-					break;
-				}
+				// quit if there's no more tables to process
+				if (!table) break;
 
-				if (command.name == "rows") {
-					string     table_name(command.argument<string>(0));
-					ColumnValues prev_key(command.argument<ColumnValues>(1));
-					ColumnValues last_key(command.argument<ColumnValues>(2));
-					handle_rows_response(table_name, prev_key, last_key);
+				// synchronize that table (unfortunately we can't share this job with other threads because next-key
+				// locking is used for unique key indexes to enforce the uniqueness constraint, so we can't share
+				// write traffic to the database, which makes it somewhat futile to try and farm the read work out)
+				sync_table(*table);
 
-				} else if (command.name == "hash") {
-					string     table_name(command.argument<string>(0));
-					ColumnValues prev_key(command.argument<ColumnValues>(1));
-					ColumnValues last_key(command.argument<ColumnValues>(2));
-					string           hash(command.argument<string>(3));
-					handle_hash_response(table_name, prev_key, last_key, hash);
+				// update our count of completed tables so we know when we've finished syncing
+				table_queue.finished_table();
+			}
 
-				} else {
-					cerr << "received unknown response: " << command.name << endl;
-					throw command_error("Unknown command " + command.name);
-				}
+			// if any of the workers aborted, exit without committing our changes, otherwise commit and return
+			if (table_queue.check_if_finished_all_tables()) {
+				client.enable_referential_integrity();
+				client.commit_transaction();
 			}
 		} catch (const exception &e) {
 			cerr << e.what() << endl;
-			work_queue.abort(); // abort all other output threads
+			table_queue.abort(); // abort all other output threads
+		}
+
+		// send a quit so the other end closes its output and terminates gracefully; note we do this both for normal completion and also errors
+		send_quit();
+	}
+
+	void negotiate_protocol() {
+		const int PROTOCOL_VERSION_SUPPORTED = 1;
+
+		// tell the other end what version of the protocol we can speak, and have them tell us which version we're able to converse in
+		send_command(output, "protocol", PROTOCOL_VERSION_SUPPORTED);
+
+		// read the response to the protocol_version command that the output thread sends when it starts
+		// this is currently unused, but the command's semantics need to be in place for it to be useful in the future...
+		input >> protocol_version;
+	}
+
+	void compare_schema() {
+		// we could do this in all workers, and it wouldn't be a bad idea to, but (especially with mysql) getting schema information
+		// is actually relatively expensive (particularly on older servers without SSDs, or on servers with many databases or tables)
+		if (leader) {
+			// get its schema
+			send_command(output, "schema");
+
+			// read the response to the schema command that the output thread sends when it starts
+			Database from_database;
+			input >> from_database;
+
+			// check they match
+			check_schema_match(from_database, client.database_schema());
 		}
 	}
 
-	void handle_rows_response(const string &table_name, const ColumnValues &prev_key, const ColumnValues &last_key) {
-		// we're being sent a range of rows; apply them to our end.  we do this in-context to
-		// provide flow control - if we buffered and used a separate apply thread, we would
-		// bloat up if this end couldn't write to disk as quickly as the other end sent data.
-		const Table &table(client.table_by_name(table_name));
-		TableRowApplier<DatabaseClient, InputStream> applier(client, input, table, prev_key, last_key);
-
-		if (last_key.empty()) {
-			// if the range extends to the end of the table, that means we're done with that table;
-			// update our count of completed tables so we know when we've finished syncing.
-			work_queue.finished_table();
-
+	void enqueue_tables() {
+		if (leader) {
+			// queue up all the tables, and signal the non-leaders that it's time to start
+			table_queue.enqueue(client.database_schema().tables);
 		} else {
-			// if it doesn't, that means we have more work to do; since we failed to match last time, don't
-			// increase the row count.  queue up (at the front of the queue so that we go deep before broad).
-			work_queue.enqueue(table, last_key, ColumnValues(), string());
+			// wait for the leader to queue the tables
+			table_queue.wait_until_started();
 		}
 	}
 
-	void handle_hash_response(const string &table_name, const ColumnValues &prev_key, const ColumnValues &last_key, const string &hash) {
-		const Table &table(client.table_by_name(table_name));
-		work_queue.enqueue(table, prev_key, last_key, hash);
+	void sync_table(const Table &table) {
+		ColumnValues prev_key;
+		ColumnValues last_key;
+		string hash;
+
+		while (true) {
+			ColumnValues matched_up_to_key;
+			size_t rows_to_hash = check_hash_and_choose_next_range(client, table, prev_key, last_key, hash, matched_up_to_key);
+
+			// calculate our hash of the next rows_to_hash rows
+			RowHasherAndLastKey<typename DatabaseClient::RowType> hasher_for_our_rows(table.primary_key_columns);
+			if (rows_to_hash) {
+				client.retrieve_rows(table, matched_up_to_key, rows_to_hash, hasher_for_our_rows);
+			}
+
+			if (hasher_for_our_rows.row_count == 0) {
+				// rows don't match, and there's only one or no rows in that range at our end, so ask the other end to send theirs
+				send_command(output, "rows", table.name, matched_up_to_key, hasher_for_our_rows.last_key /* empty, meaning to the end of the table */);
+
+			} else {
+				// tell the other end to check its hash of the same rows, using key ranges rather than a count to improve the chances of a match.
+				send_command(output, "hash", table.name, matched_up_to_key, hasher_for_our_rows.last_key, hasher_for_our_rows.finish());
+			}
+
+			Command command;
+			input >> command;
+
+			if (command.name == "hash") {
+				if (command.argument<string>(0) != table.name) throw command_error("Received response on table " + command.argument<string>(0) + " but request was for " + table.name);
+
+				// they've sent us back a hash for a set of rows, which will happen if:
+				// - the last hash we sent them matched, and so they've moved on to the next set of rows; or
+				// - the last hash we sent them didn't match, so they've reduced the key range and sent us back
+				//   the hash for a smaller set of rows (but not so small that they sent back the data instead)
+				// we don't need to know which case it is; simply loop around and carry on
+				prev_key = command.argument<ColumnValues>(1);
+				last_key = command.argument<ColumnValues>(2);
+				hash     = command.argument<string>(3);
+
+			} else if (command.name == "rows") {
+				if (command.argument<string>(0) != table.name) throw command_error("Received response on table " + command.argument<string>(0) + " but request was for " + table.name);
+
+				// we're being sent a range of rows; apply them to our end.  we do this in-context to
+				// provide flow control - if we buffered and used a separate apply thread, we would
+				// bloat up if this end couldn't write to disk as quickly as the other end sent data.
+				prev_key = command.argument<ColumnValues>(1);
+				last_key = command.argument<ColumnValues>(2);
+
+				TableRowApplier<DatabaseClient, FDReadStream> applier(client, input, table, prev_key, last_key);
+
+				// if the range extends to the end of their table, that means we're done with that table
+				if (last_key.empty()) return;
+
+				// if it doesn't, that means they have more rows after these ones, so more work to do;
+				// since we failed to match last time, don't increase the row count.
+				prev_key = last_key;
+				last_key = ColumnValues();
+				hash     = string();
+
+			} else {
+				throw command_error("Unknown command " + command.name);
+			}
+		}
 	}
 
-	SyncWorkQueue &work_queue;
+	void send_quit() {
+		try {
+			send_command(output, "quit");
+		} catch (const exception &e) {
+			// we don't care if sending this command fails itself, we're already past the point where we could abort anyway
+		}
+	}
+
+	SyncTableQueue &table_queue;
 	DatabaseClient client;
-	InputStream input_stream;
-	Unpacker<InputStream> input;
+	FDWriteStream output_stream;
+	FDReadStream input_stream;
+	Unpacker<FDReadStream> input;
+	Packer<FDWriteStream> output;
 	bool leader;
 	int protocol_version;
 	boost::thread worker_thread;
@@ -208,23 +196,19 @@ struct SyncToInputWorker {
 
 template <typename DatabaseClient>
 void sync_to(const char *database_host, const char *database_port, const char *database_name, const char *database_username, const char *database_password, int num_workers, int startfd) {
-	SyncWorkQueue work_queue;
-	vector< SyncToInputWorker<DatabaseClient, FDReadStream>* > input_workers;
-	vector< SyncToOutputWorker<DatabaseClient, FDWriteStream>* > output_workers;
+	SyncTableQueue table_queue;
+	vector<SyncToWorker<DatabaseClient>*> workers;
 
-	output_workers.resize(num_workers);
-	input_workers.resize(num_workers);
+	workers.resize(num_workers);
 
 	for (int worker = 0; worker < num_workers; worker++) {
 		bool leader = (worker == 0);
 		int read_from_descriptor = startfd + worker;
 		int write_to_descriptor = startfd + worker + num_workers;
-		input_workers[worker] = new SyncToInputWorker<DatabaseClient, FDReadStream>(work_queue, database_host, database_port, database_name, database_username, database_password, read_from_descriptor, leader);
-		output_workers[worker] = new SyncToOutputWorker<DatabaseClient, FDWriteStream>(work_queue, database_host, database_port, database_name, database_username, database_password, write_to_descriptor, leader);
+		workers[worker] = new SyncToWorker<DatabaseClient>(table_queue, database_host, database_port, database_name, database_username, database_password, read_from_descriptor, write_to_descriptor, leader);
 	}
 
-	for (typename vector< SyncToInputWorker<DatabaseClient, FDReadStream>* >::const_iterator it = input_workers.begin(); it != input_workers.end(); ++it) delete *it;
-	for (typename vector< SyncToOutputWorker<DatabaseClient, FDWriteStream>* >::const_iterator it = output_workers.begin(); it != output_workers.end(); ++it) delete *it;
+	for (typename vector<SyncToWorker<DatabaseClient>*>::const_iterator it = workers.begin(); it != workers.end(); ++it) delete *it;
 
-	if (work_queue.aborted) throw sync_error();
+	if (table_queue.aborted) throw sync_error();
 }
