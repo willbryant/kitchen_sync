33df65e3bce313f9f2954befb9a200f227d53d9d
Will Bryant <will.bryant@gmail.com>
2014-01-11 00:48:50 +1300

    use uglier DELETE query search terms to work around very slow unique index clearing due to http://bugs.mysql.com/bug.php?id=31188

--- src/table_row_applier.h
+++ src/table_row_applier.h
@@ -45,8 +45,9 @@ template <typename DatabaseClient>
 struct UniqueKeyClearer {
 	UniqueKeyClearer(DatabaseClient &client, const Table &table, const Key &key):
 		client(&client),
+		table(&table),
 		columns(&key.columns),
-		delete_sql("DELETE FROM " + table.name + " WHERE " + columns_list(table.columns, key.columns) + " IN (\n(", "))") {
+		delete_sql("DELETE FROM " + table.name + " WHERE (", ")") {
 	}
 
 	bool key_enforceable(const NullableRow &row) {
@@ -60,13 +61,17 @@ struct UniqueKeyClearer {
 		// rows with any NULL values won't enforce a uniqueness constraint, so we don't need to clear them
 		if (!key_enforceable(row)) return;
 
-		if (delete_sql.have_content()) delete_sql += "),\n(";
+		if (delete_sql.have_content()) delete_sql += ")\nOR (";
 		for (size_t n = 0; n < columns->size(); n++) {
+			// frustratingly http://bugs.mysql.com/bug.php?id=31188 was not fixed until 5.7.3 so we can't simply make a big WHERE (key columns) IN (tuples) here, and have to use AND/OR repetition instead
 			if (n > 0) {
-				delete_sql += ',';
+				delete_sql += " AND ";
 			}
+			size_t column = (*columns)[n];
+			delete_sql += table->columns[column].name;
+			delete_sql += '=';
 			delete_sql += '\'';
-			delete_sql += client->escape_value(row[(*columns)[n]].value);
+			delete_sql += client->escape_value(row[column].value);
 			delete_sql += '\'';
 		}
 	}
@@ -75,9 +80,10 @@ struct UniqueKeyClearer {
 		delete_sql.apply(*client);
 	}
 
-	// these two should both be references, but g++ 4.6's STL needs vector element types to be Assignable,
+	// these three should both be references, but g++ 4.6's STL needs vector element types to be Assignable,
 	// which is impossible with references.
 	DatabaseClient *client;
+	const Table *table;
 	const ColumnIndices *columns;
 	BaseSQL delete_sql;
 };
