270d39616efdd56f99c24d5beb52eccc280d1950
Will Bryant <will.bryant@gmail.com>
2014-01-23 23:11:38 +1300

    explicitly list the columns to select

--- src/database_client.cpp
+++ src/database_client.cpp
@@ -8,7 +8,12 @@
 string DatabaseClient::retrieve_rows_sql(const Table &table, const ColumnValues &prev_key, size_t row_count) {
 	string key_columns(columns_list(table.columns, table.primary_key_columns));
 
-	string result("SELECT * FROM ");
+	string result("SELECT ");
+	for (Columns::const_iterator column = table.columns.begin(); column != table.columns.end(); ++column) {
+		if (column != table.columns.begin()) result += ", ";
+		result += column->name;
+	}
+	result += " FROM ";
 	result += table.name;
 	if (!prev_key.empty()) {
 		result += " WHERE " + key_columns + " > " + non_binary_string_values_list(prev_key);
@@ -21,7 +26,12 @@ string DatabaseClient::retrieve_rows_sql(const Table &table, const ColumnValues 
 string DatabaseClient::retrieve_rows_sql(const Table &table, const ColumnValues &prev_key, const ColumnValues &last_key) {
 	string key_columns(columns_list(table.columns, table.primary_key_columns));
 
-	string result("SELECT * FROM ");
+	string result("SELECT ");
+	for (Columns::const_iterator column = table.columns.begin(); column != table.columns.end(); ++column) {
+		if (column != table.columns.begin()) result += ", ";
+		result += column->name;
+	}
+	result += " FROM ";
 	result += table.name;
 	result += where_sql(key_columns, prev_key, last_key);
 	result += + " ORDER BY " + key_columns.substr(1, key_columns.size() - 2);
