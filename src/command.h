ac47c9889fe0664858180f583b980d2f20b0d98c
Will Bryant <will.bryant@gmail.com>
2014-02-10 01:24:45 +1300

    introduce a typedef for command verbs to help the self-documentation of forthcoming code

--- src/command.h
+++ src/command.h
@@ -7,8 +7,10 @@
 
 using namespace std;
 
+typedef uint32_t verb_t;
+
 struct Command {
-	uint32_t verb;
+	verb_t verb;
 	vector<boost::any> arguments;
 
 	template<class T>
@@ -24,7 +26,7 @@ Command &operator >> (Unpacker<InputStream> &unpacker, Command &command) {
 	size_t array_length = unpacker.next_array_length(); // checks type
 	if (array_length < 1) throw logic_error("Expected at least one element when reading command");
 
-	command.verb = unpacker.template next<uint32_t>();
+	command.verb = unpacker.template next<verb_t>();
 	command.arguments.clear();
 	while (--array_length) {
 		command.arguments.push_back(unpacker.template next<boost::any>());
@@ -45,7 +47,7 @@ inline void send_values(Packer<OutputStream> &packer, const T &arg0, const Value
 }
 
 template <typename OutputStream, typename... Values>
-void send_command(Packer<OutputStream> &packer, uint32_t verb, const Values &...args) {
+void send_command(Packer<OutputStream> &packer, verb_t verb, const Values &...args) {
 	packer.pack_array_length(1 + sizeof...(Values)); /* verb + number of args */
 	packer << verb;
 	send_values(packer, args...);
@@ -57,17 +59,17 @@ struct command_error: public runtime_error {
 };
 
 namespace Commands {
-	const uint32_t OPEN = 1;
-	const uint32_t ROWS = 2;
-	const uint32_t HASH = 3;
+	const verb_t OPEN = 1;
+	const verb_t ROWS = 2;
+	const verb_t HASH = 3;
 
-	const uint32_t PROTOCOL = 32;
-	const uint32_t EXPORT_SNAPSHOT  = 33;
-	const uint32_t IMPORT_SNAPSHOT  = 34;
-	const uint32_t UNHOLD_SNAPSHOT  = 35;
-	const uint32_t WITHOUT_SNAPSHOT = 36;
-	const uint32_t SCHEMA = 37;
-	const uint32_t QUIT = 0;
+	const verb_t PROTOCOL = 32;
+	const verb_t EXPORT_SNAPSHOT  = 33;
+	const verb_t IMPORT_SNAPSHOT  = 34;
+	const verb_t UNHOLD_SNAPSHOT  = 35;
+	const verb_t WITHOUT_SNAPSHOT = 36;
+	const verb_t SCHEMA = 37;
+	const verb_t QUIT = 0;
 };
 
 #endif
