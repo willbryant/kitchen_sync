aa1b3af9bddc9fd43d4c6cf07b23c3da72efc489
Will Bryant <will.bryant@gmail.com>
2014-04-21 22:02:30 +1200

    use allocated byte buffers instead of vectors for PackedValue, wins back all or most of the performance loss on this branch

--- src/message_pack/packed_value.h
+++ src/message_pack/packed_value.h
@@ -2,37 +2,85 @@
 #define PACKED_VALUE
 
 #include <string.h>
+#include <stdlib.h>
+#include <stdexcept>
 #include "type_codes.h"
 
 struct PackedValue {
-	std::vector<uint8_t> encoded_bytes;
+	PackedValue(): encoded_bytes(NULL), used(0) {}
+
+	~PackedValue() {
+		free(encoded_bytes);
+	}
+
+	PackedValue(const PackedValue &from): encoded_bytes(NULL) {
+		*this = from;
+	}
+
+	PackedValue(PackedValue &&from): encoded_bytes(NULL) {
+		*this = std::move(from);
+	}
+
+	PackedValue &operator=(const PackedValue &from) {
+		free(encoded_bytes);
+		encoded_bytes = NULL;
+		used = from.used;
+		if (used) {
+			encoded_bytes = (uint8_t *)malloc(used);
+			if (!encoded_bytes) throw std::bad_alloc();
+			memcpy(encoded_bytes, from.encoded_bytes, used);
+		}
+		return *this;
+	}
+
+	PackedValue &operator=(PackedValue &&from) {
+		if (this != &from) {
+			free(encoded_bytes);
+			used = from.used;
+			encoded_bytes = from.encoded_bytes;
+			from.encoded_bytes = NULL;
+			from.used = 0;
+		}
+		return *this;
+	}
 
 	inline uint8_t *extend(size_t bytes) {
-		size_t size_before = encoded_bytes.size();
-		encoded_bytes.resize(size_before + bytes);
-		return encoded_bytes.data() + size_before;
+		size_t size_before = used;
+		used += bytes;
+		encoded_bytes = (uint8_t *)realloc(encoded_bytes, used);
+		if (!encoded_bytes) throw std::bad_alloc();
+		return encoded_bytes + size_before;
+	}
+
+	inline void clear() {
+		free(encoded_bytes);
+		encoded_bytes = NULL;
+		used = 0;
 	}
 
 	inline void write(const uint8_t *src, size_t bytes) {
 		memcpy(extend(bytes), src, bytes);
 	}
 
-	inline void clear() { encoded_bytes.clear(); }
-
-	inline bool empty() const { return encoded_bytes.empty(); }
-	inline size_t size() const { return encoded_bytes.size(); }
-	inline uint8_t leader() const { return (encoded_bytes.empty() ? 0 : encoded_bytes.front()); }
-	inline const uint8_t *data() const { return encoded_bytes.data(); }
+	inline bool empty() const { return !used; }
+	inline size_t size() const { return used; }
+	inline uint8_t leader() const { return (used ? *encoded_bytes : 0); }
+	inline const uint8_t *data() const { return encoded_bytes; }
 
 	inline bool is_nil() const { return (leader() == MSGPACK_NIL); }
 
 	inline bool operator == (const PackedValue &other) const {
-		return (encoded_bytes == other.encoded_bytes);
+		return (used == other.used && memcmp(encoded_bytes, other.encoded_bytes, used) == 0);
 	}
 
 	inline bool operator < (const PackedValue &other) const {
-		return (encoded_bytes < other.encoded_bytes);
+		if (used != other.used) return (used < other.used);
+		return (memcmp(encoded_bytes, other.encoded_bytes, used) < 0);
 	}
+
+protected:
+	uint8_t *encoded_bytes;
+	size_t used;
 };
 
 #endif
