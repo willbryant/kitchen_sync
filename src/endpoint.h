c625d2ee45d5eafca45f4ac1b7004070ffe1fa29
Will Bryant <will.bryant@gmail.com>
2015-01-19 15:49:50 +1300

    copy endpoint database args earlier so they can't get overwritten

--- src/endpoint.h
+++ src/endpoint.h
@@ -22,12 +22,12 @@ int endpoint_main(int argc, char *argv[]) {
 
 	try {
 		// the first set of arguments are the same for both endpoints
-		const char *database_host(argv[2]);
-		const char *database_port(argv[3]);
-		const char *database_name(argv[4]);
-		const char *database_username(argv[5]);
-		const char *database_password(argv[6]);
-		const char *set_variables(argv[7]);
+		string database_host(argv[2]);
+		string database_port(argv[3]);
+		string database_name(argv[4]);
+		string database_username(argv[5]);
+		string database_password(argv[6]);
+		string set_variables(argv[7]);
 
 		// unfortunately when we transport program arguments over SSH it flattens them into a string and so empty arguments get lost; we work around by using "-"
 		if (database_port     == string("-")) database_port     = "";
