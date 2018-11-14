855b00dd5ccfdf7283150abb0f40f269db0df4c3
Will Bryant <will.bryant@gmail.com>
2018-11-14 23:27:04 +1300

    remove unused old constants

--- src/defaults.h
+++ src/defaults.h
@@ -5,9 +5,6 @@
 
 const HashAlgorithm DEFAULT_HASH_ALGORITHM = HashAlgorithm::md5; // can be overridden by command-line option
 
-const size_t DEFAULT_MINIMUM_BLOCK_SIZE6 =      256*1024; // arbitrary, but needs to be big enough to cope with a moderate amount of latency
-const size_t DEFAULT_MAXIMUM_BLOCK_SIZE6 = 256*1024*1024; // arbitrary, but needs to be small enough we don't waste unjustifiable amounts of CPU time if a block hash doesn't match
-
 const size_t DEFAULT_MINIMUM_BLOCK_SIZE =      16*1024; // arbitrary, latency isn't as big a problem with pipelining, but this sets the point at which we stop re-hashing and send rows
 const size_t DEFAULT_MAXIMUM_BLOCK_SIZE = 64*1024*1024; // arbitrary, but needs to be small enough we don't waste unjustifiable amounts of CPU time if a block hash doesn't match
 
