16e0131f2d37cfc7695e867ff7549463960ddfc3
Will Bryant <will.bryant@gmail.com>
2019-04-24 01:05:46 +1200

    refactor to use a more-idiomatic predicate object in preparation for reuse

--- src/substitute_primary_key.h
+++ src/substitute_primary_key.h
@@ -1,20 +1,25 @@
 #ifndef SUBSTITUTE_PRIMARY_KEY_H
 #define SUBSTITUTE_PRIMARY_KEY_H
 
-inline bool any_column_nullable(const Table &table, const ColumnIndices &columns) {
-	for (size_t column : columns) {
-		if (table.columns[column].nullable) return true;
+struct ColumnNullable {
+	ColumnNullable(const Table &table): table(table) {}
+
+	bool operator()(size_t column) {
+		return table.columns[column].nullable;
 	}
-	return false;
-}
+
+	const Table &table;
+};
 
 inline void choose_primary_key_for(Table &table) {
 	// generally we expect most tables to have a real primary key
 	if (table.primary_key_type == explicit_primary_key) return;
 
+	ColumnNullable nullable(table);
+
 	// if not, we need to find a unique key with no nullable columns to act as a surrogate primary key
 	for (const Key &key : table.keys) {
-		if (key.unique && !any_column_nullable(table, key.columns)) {
+		if (key.unique && none_of(key.columns.begin(), key.columns.end(), nullable)) {
 			table.primary_key_columns = key.columns;
 			table.primary_key_type = suitable_unique_key;
 			return;
