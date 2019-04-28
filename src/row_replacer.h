7803ebe7620f6b69fa3f2c564334a2dd222da8f3
Will Bryant <will.bryant@gmail.com>
2019-04-28 23:46:16 +1200

    fold in the RowReplacer specialisation in preparation for adding extra forthcoming cases
    after the previous commit the two had become essentially the same, except for the loop in insert_row and replace_row which will immediately terminate in the SupportsReplace case; benchmarking showed no performance difference so it isn't worth maintaining two variants.
    
    features in the pipeline will require us to switch back to the base case for certain tables, so the decision between the two cases won't always be able to be made at compile time.

--- src/row_replacer.h
+++ src/row_replacer.h
@@ -20,34 +20,64 @@ void append_row_tuple(DatabaseClient &client, const Columns &columns, BaseSQL &s
 
 typedef std::function<void ()> ProgressCallback;
 
-// databases that don't support the REPLACE statement must explicitly clear conflicting rows
+template <typename DatabaseClient>
+struct RowReplacer;
+
 template <typename DatabaseClient, bool = is_base_of<SupportsReplace, DatabaseClient>::value>
+struct RowReplacerBuilder {
+	static const char *insert_sql_base() {
+		return "INSERT INTO ";
+	}
+
+	static void construct_clearers(RowReplacer<DatabaseClient> &row_replacer) {
+		// databases that don't support the REPLACE statement must explicitly clear conflicting rows
+		for (const Key &key : row_replacer.table.keys) {
+			if (key.unique) {
+				row_replacer.unique_key_clearers.emplace_back(row_replacer.client, row_replacer.table, key.columns);
+			}
+		}
+
+		row_replacer.replace_clearers_start = row_replacer.unique_key_clearers.begin();
+		row_replacer.insert_clearers_start = row_replacer.unique_key_clearers.begin() + 1;
+	}
+};
+
+template <typename DatabaseClient>
+struct RowReplacerBuilder<DatabaseClient, true> {
+	static const char *insert_sql_base() {
+		return "REPLACE INTO ";
+	}
+
+	static void construct_clearers(RowReplacer<DatabaseClient> &row_replacer) {
+		// databases that support the REPLACE statement will clear any conflicting rows automatically
+		row_replacer.replace_clearers_start = row_replacer.unique_key_clearers.end();
+		row_replacer.insert_clearers_start = row_replacer.unique_key_clearers.end();
+	}
+};
+
+template <typename DatabaseClient>
 struct RowReplacer {
 	RowReplacer(DatabaseClient &client, const Table &table, bool commit_often, ProgressCallback progress_callback):
 		client(client),
-		columns(table.columns),
-		insert_sql("INSERT INTO " + table.name + " VALUES\n(", ")"),
+		table(table),
+		insert_sql(RowReplacerBuilder<DatabaseClient>::insert_sql_base() + table.name + " VALUES\n(", ")"),
 		commit_often(commit_often),
 		progress_callback(progress_callback),
 		rows_changed(0) {
 		// set up the clearers we'll need to insert rows - these clear any conflicting values from elsewhere in the same table
 		unique_key_clearers.emplace_back(client, table, table.primary_key_columns);
-		for (const Key &key : table.keys) {
-			if (key.unique) {
-				unique_key_clearers.emplace_back(client, table, key.columns);
-			}
-		}
+		RowReplacerBuilder<DatabaseClient>::construct_clearers(*this);
 	}
 
-	void insert_row(const PackedRow &row) {
+	inline void insert_row(const PackedRow &row) {
 		// before we can insert our rows we will also have to first clear any other rows with the
 		// same unique key values.
-		for (auto unique_key_clearer = unique_key_clearers.begin() + 1; unique_key_clearer != unique_key_clearers.end(); ++unique_key_clearer) {
+		for (auto unique_key_clearer = insert_clearers_start; unique_key_clearer != unique_key_clearers.end(); ++unique_key_clearer) {
 			unique_key_clearer->row(row);
 		}
 
 		// we can then batch up a big INSERT statement
-		append_row_tuple(client, columns, insert_sql, row);
+		append_row_tuple(client, table.columns, insert_sql, row);
 
 		rows_changed++;
 	}
@@ -55,11 +85,11 @@ struct RowReplacer {
 	inline void replace_row(const PackedRow &row) {
 		// when we apply(), first we will delete existing rows - we do that rather than use UPDATE
 		// statements because you can't really batch UPDATE, whereas you can batch DELETE & INSERT.
-		for (auto unique_key_clearer = unique_key_clearers.begin(); unique_key_clearer != unique_key_clearers.end(); ++unique_key_clearer) {
+		for (auto unique_key_clearer = replace_clearers_start; unique_key_clearer != unique_key_clearers.end(); ++unique_key_clearer) {
 			unique_key_clearer->row(row);
 		}
 
-		append_row_tuple(client, columns, insert_sql, row);
+		append_row_tuple(client, table.columns, insert_sql, row);
 
 		rows_changed++;
 	}
@@ -70,7 +100,7 @@ struct RowReplacer {
 		rows_changed++;
 	}
 
-	inline void apply() {
+	void apply() {
 		for (UniqueKeyClearer<DatabaseClient> &unique_key_clearer : unique_key_clearers) {
 			unique_key_clearer.apply();
 		}
@@ -88,61 +118,11 @@ struct RowReplacer {
 	}
 
 	DatabaseClient &client;
-	const Columns &columns;
+	const Table &table;
 	BaseSQL insert_sql;
 	vector< UniqueKeyClearer<DatabaseClient> > unique_key_clearers;
-	bool commit_often;
-	ProgressCallback progress_callback;
-	size_t rows_changed;
-};
-
-// databases that do support REPLACE are much simpler - we just use the same statement for any type of insert/update
-template <typename DatabaseClient>
-struct RowReplacer<DatabaseClient, true> {
-	RowReplacer(DatabaseClient &client, const Table &table, bool commit_often, ProgressCallback progress_callback):
-		client(client),
-		columns(table.columns),
-		insert_sql("REPLACE INTO " + table.name + " VALUES\n(", ")"),
-		commit_often(commit_often),
-		rows_changed(0) {
-		unique_key_clearers.emplace_back(client, table, table.primary_key_columns);
-	}
-
-	inline void insert_row(const PackedRow &row) {
-		replace_row(row);
-	}
-
-	inline void replace_row(const PackedRow &row) {
-		append_row_tuple(client, columns, insert_sql, row);
-
-		rows_changed++;
-	}
-
-	inline void remove_row(const PackedRow &row) {
-		unique_key_clearers.front().row(row);
-
-		rows_changed++;
-	}
-
-	inline void apply() {
-		unique_key_clearers.front().apply();
-
-		insert_sql.apply(client);
-
-		if (commit_often) {
-			client.commit_transaction();
-			client.start_write_transaction();
-		}
-
-		if (progress_callback) {
-			progress_callback();
-		}
-	}
-
-	DatabaseClient &client;
-	const Columns &columns;
-	BaseSQL insert_sql;
-	vector< UniqueKeyClearer<DatabaseClient> > unique_key_clearers;
+	typename vector< UniqueKeyClearer<DatabaseClient> >::iterator insert_clearers_start;
+	typename vector< UniqueKeyClearer<DatabaseClient> >::iterator replace_clearers_start;
 	bool commit_often;
 	ProgressCallback progress_callback;
 	size_t rows_changed;
