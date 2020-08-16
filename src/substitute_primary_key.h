6619d406f06ab2a8e811d13495c37db86ceac6ab
Will Bryant <will.bryant@gmail.com>
2020-08-16 21:44:22 +1200

    Fix an incorrect variable name

--- src/substitute_primary_key.h
+++ src/substitute_primary_key.h
@@ -21,13 +21,13 @@ inline void choose_primary_key_for(Table &table) {
 	// generally we expect most tables to have a real primary key
 	if (table.primary_key_type == PrimaryKeyType::explicit_primary_key) return;
 
-	ColumnNullableOrReplaced usable(table);
+	ColumnNullableOrReplaced unusable(table);
 
 	// if not, we want to find a unique key with no nullable columns to act as a surrogate primary key.
 	// we also need to ensure there's no replace: expression applied as the WHERE conditions would
 	// apply to the actual values, which presumably don't match the replaced values.
 	for (const Key &key : table.keys) {
-		if (key.unique() && none_of(key.columns.begin(), key.columns.end(), usable)) {
+		if (key.unique() && none_of(key.columns.begin(), key.columns.end(), unusable)) {
 			table.primary_key_columns = key.columns;
 			table.primary_key_type = PrimaryKeyType::suitable_unique_key;
 			return;
@@ -38,7 +38,7 @@ inline void choose_primary_key_for(Table &table) {
 	// if it were the primary key and group and count to spot duplicates.  that's only possible if
 	// there are no nullable columns, though; otherwise we can't query based on key ranges, since the
 	// comparison operators like > and <= will return NULL for any comparisons involving NULL values.
-	if (none_of(table.columns.begin(), table.columns.end(), usable)) {
+	if (none_of(table.columns.begin(), table.columns.end(), unusable)) {
 		// ok, no nullable columns, so in principle we can use the whole row as its own primary key.
 		// but tables like that are potentially very slow to query because the database may not have
 		// any good way to sort the rows, and we can't assume that it will happen to serve them up
