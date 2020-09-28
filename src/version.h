732ba1109093535f98e92ce38c4eb1b2b7490e5e
Will Bryant <will.bryant@gmail.com>
2020-09-28 23:53:35 +1300

    Fix merge of v2.6

--- src/version.h
+++ src/version.h
@@ -1,18 +1,1 @@
-#ifndef VERSION_H
-#define VERSION_H
-
-#include <sstream>
-
-#define KS_MAJOR_VERSION 2
-#define KS_MINOR_VERSION 6
-#define KS_MAINT_VERSION 0
-
-inline string ks_version() {
-	std::ostringstream string_stream;
-
-	string_stream << KS_MAJOR_VERSION << '.' << KS_MINOR_VERSION << '.' << KS_MAINT_VERSION;
-
-	return string_stream.str();
-}
-
-#endif
+#define KS_VERSION "2.6"
